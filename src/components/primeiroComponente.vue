eb68fb4256aa68785b7285ce9c36d91290f735f7
Guilherme Moraes <guilhermemoraes.dev@gmail.com>
2022-02-08 18:27:42 -0300

    first component

---
+++ src/components/primeiroComponente.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Olá Vue! Isso é um componente dinânico</h1>
+</template>
+
+<script>
+export default {
+  name: "primeiroComponente", // Definindo o nome do componente
+};
+</script>
