e483eaf613d82e034782d42c9864918c2e054a9b
Guilherme Moraes <guilhermemoraes.dev@gmail.com>
2022-02-08 18:45:29 -0300

    data

--- src/components/primeiroComponente.vue
+++ src/components/primeiroComponente.vue
@@ -1,9 +1,20 @@
 <template>
   <h1>Olá Vue! Isso é um componente dinânico</h1>
+  <p>
+    Meu nome é <code>{{ nome }}</code> e trabalho como
+    <code>{{ profissao }}</code>
+  </p>
 </template>
 
 <script>
 export default {
   name: "primeiroComponente", // Definindo o nome do componente
+  data() {
+    // Todos os dados são declarados dentro de data()
+    return {
+      nome: "Guilherme",
+      profissao: "Programador",
+    };
+  },
 };
 </script>
