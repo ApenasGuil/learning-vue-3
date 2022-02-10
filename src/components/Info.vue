e670e7256295683947ed3329f50933799290dea8
Guilherme Moraes <guilhermemoraes.dev@gmail.com>
2022-02-10 13:36:11 -0300

    v-else

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,5 +1,6 @@
 <template>
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades!</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
         <li>JavaScript</li>
