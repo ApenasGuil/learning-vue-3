cc01f4c26c083ab46ee042bf1686b5cb6781d714
Guilherme Moraes <guilhermemoraes.dev@gmail.com>
2022-03-01 21:41:38 -0300

    multiple events

--- src/components/primeiroComponente.vue
+++ src/components/primeiroComponente.vue
@@ -1,20 +1,25 @@
 <template>
-  <h1>Olá Vue! Isso é um componente dinânico</h1>
-  <p>
-    Meu nome é <code>{{ nome }}</code> e trabalho como
-    <code>{{ profissao }}</code>
-  </p>
+    <h1>Olá Vue! Isso é um componente dinânico</h1>
+    <p>
+        Meu nome é <code>{{ nome }}</code> e trabalho como
+        <code>{{ profissao }}</code>
+    </p>
+    <MultiplosEventos />
 </template>
 
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue";
 export default {
-  name: "PrimeiroComponente", // Definindo o nome do componente
-  data() {
-    // Todos os dados são declarados dentro de data()
-    return {
-      nome: "Guilherme",
-      profissao: "Programador",
-    };
-  },
+    name: "PrimeiroComponente", // Definindo o nome do componente
+    components: {
+        MultiplosEventos,
+    },
+    data() {
+        // Todos os dados são declarados dentro de data()
+        return {
+            nome: "Guilherme",
+            profissao: "Programador",
+        };
+    },
 };
 </script>
